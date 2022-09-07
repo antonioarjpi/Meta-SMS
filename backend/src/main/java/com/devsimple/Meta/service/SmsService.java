70afd0c1b4382ca960693ef0b94925b2f4a38729
unknown <antonioarjpi@gmail.com>
2022-09-07 17:40:14 -0300

    Profile PROD

--- backend/src/main/java/com/devsimple/Meta/service/SmsService.java
+++ backend/src/main/java/com/devsimple/Meta/service/SmsService.java
@@ -5,7 +5,6 @@ import com.devsimple.Meta.repository.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
